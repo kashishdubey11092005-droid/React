63c359b2b446d57b74ca1b9ba4a98f0a11635e92
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-07 21:28:08 +0000

    react app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello, Vite + React! kashish</h1>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
