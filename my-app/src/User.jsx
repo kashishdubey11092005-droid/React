4abc798c191827cc19140038bc23d6ef805967c6
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-11 10:22:11 +0000

    Toggle

--- my-app/src/User.jsx
+++ my-app/src/User.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
+// function User() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <h2>Counter</h2>
+//       <p>Count value: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   )
+// }
+
+// export default User;
 function User() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <h2>Counter</h2>
-      <p>Count value: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>User Component</h1>
     </div>
   )
 }
 
-export default User;
+export default User
