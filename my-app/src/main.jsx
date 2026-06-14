a7af44c3bbe48be9c52c88e2f6b3d43a06b21086
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-14 11:48:27 +0000

    Increment_Decrement Button

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Button from './Button.jsx'
+import IncrementDecrement from './increment_Decrement.jsx'
+import Toggle from './Toggle.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Button />
+    <IncrementDecrement />
+  </StrictMode>
 )
