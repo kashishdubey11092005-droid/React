eaee07a1934255f313e986c9a8298a58ea98083c
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-14 09:17:47 +0000

    increment decrement

---
+++ my-app/src/increment Decrement.jsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+const App = ()=> {
+const[ num,  setNum] = useState(10)
+return(
+<div>
+<h1> Number is {num} </h1>
+<button onClick={ ()=> setNum(Num+10)}>  </button>
+<button onClick={()=> setNum(num-10)}></button>
+
+
+
+</div>
+)
+}
+export default App
+
