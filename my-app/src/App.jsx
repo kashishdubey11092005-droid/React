fe6b141c4629557b2aae15cec3b260f17ef746dd
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-17 10:41:52 +0000

    tailwind o/p

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-const App  = () => {
-   const user = "sarthak"
-   const age= 100
-   return (
-    <div>   {user}, {age}  </div>
-   )
+
+function App() {
+  return (
+    <div className="text-5xl font-bold text-blue-500">
+      Kashish the full stack developer
+      <img src = "https://unsplash.com/@brookebalentine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="Brooke Balentine" /> 
+      
+      
+    </div>
+  );
 }
-export default App
+
+export default App;
+
