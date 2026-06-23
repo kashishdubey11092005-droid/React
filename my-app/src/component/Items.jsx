866bf8bcdd041381c5a2147efd094cf1e13c4cdd
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-23 15:21:56 +0000

    && operator

--- my-app/src/component/Items.jsx
+++ my-app/src/component/Items.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 const Items = () => {
     const age= 20 ;
+    const aadhar = false;
+    const gmail = true;
+
     return (
-        <div>
-      { age>=18 ? (
-        <>
-        <h1> you can drive </h1>
-        </>
-        ):(
-         <>
-         <h1>
-            you are not eligible for drive
-         </h1>
-         </>
-        )
-      }
-      </div>
+   <>
+   { aadhar && <h1>   you can open Bank Account </h1>}
+   { gmail  && <h1> you can make youtube channel </h1>}
+   
+   </>
+ //     <div>
+    //   { age>=18 ? (
+    //     <>
+    //     <h1> you can drive </h1>
+    //     </>
+    //     ):(
+    //      <>
+    //      <h1>
+    //         you are not eligible for drive
+    //      </h1>
+    //      </>
+    //     )
+    //   }
+    //   </div>
     )
 }
  export default Items;    
