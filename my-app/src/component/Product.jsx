6703421169c80b5416cc640ae0f4dd0872e7f894
kashish dubey <kashishdubey11092005@gmail.com>
2026-06-24 20:32:24 +0000

    inline css implementing

--- my-app/src/component/Product.jsx
+++ my-app/src/component/Product.jsx
@@ -4,7 +4,15 @@ const product = ({name,price,color,data ={ } }) => {
     console.log(data.ram);
 
     return(
-        <div>
+        <div
+      style = { { 
+      backgroundColor : "blue",
+      padding : "10px",
+      margin : "10px",
+      border : "1px solid yellow ",
+      border : "10px",
+
+      } } >
         <h1>Brand - {name}</h1>
         <h2>Price - {price}</h2>
         <h3>Color - {color}</h3>
